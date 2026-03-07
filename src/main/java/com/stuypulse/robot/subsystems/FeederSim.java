bd640e1df794a1d27a343b73b71e1f072dce678a
Aff-n <affanibnajasim@gmail.com>
2026-03-07 15:21:47 -0500

    FEAT: FeederSim

--- src/main/java/com/stuypulse/robot/subsystems/FeederSim.java
+++ src/main/java/com/stuypulse/robot/subsystems/FeederSim.java
@@ -1,5 +1,30 @@
 package com.stuypulse.robot.subsystems;
 
+import com.stuypulse.robot.constants.Settings;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+
 public class FeederSim extends Feeder{
     
+    private FlywheelSim feeder; 
+
+    public FeederSim(){
+        super();
+        
+        DCMotor motor = DCMotor.getCIM(1);
+        feeder = new FlywheelSim(
+            LinearSystemId.createFlywheelSystem(
+            motor,
+            1.9,
+            2),
+        motor
+        );
+    }
+
+    @Override
+    public void periodic(){
+        super.periodic();
+        feeder.update(Settings.DT);
+    }
 }
